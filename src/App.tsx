10150e794144e299768b663a7e725353672d5102
DvorakRadek <radekdvorak90@gmail.com>
2025-02-13 09:12:10 +0100

    updated layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Board from "./components/Board"
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-gray-200 text-black">
+    <div className="w-screen min-h-screen flex justify-center items-center bg-gray-200 text-black">
       <Board />
     </div>
   )
